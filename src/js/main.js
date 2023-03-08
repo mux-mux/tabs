578f32ae1548a768d3e4c380e3d4d3042d5a4ad7
mikemouse <kmmyemail@ukr.net>
2023-03-08 14:00:57 +0200

    feat: add tabs

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(content => {
+      content.classList.add('sidepanel__hide');
+      content.classList.remove('sidepanel__show', 'sidepanel__fade');
+    });
+
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('sidepanel__show', 'sidepanel__fade');
+    tabsContent[i].classList.remove('sidepanel__hide');
+    tabs[i].classList.add('tabheader__item_active');
+  };
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((tab, i) => {
+        if (target == tab) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  })
+});
